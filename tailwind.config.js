0945e7425201331c4e62201365dff386fc5e5d9f
Jeferson Fernandes <jeferson.fernandes@maxiconsystems.com.br>
2024-03-25 20:50:46 -0300

    FEAT: configuration of tailwind base theme and import asset with images and videos

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: "#2997FF",
+        gray: {
+          DEFAULT: "#86868B",
+          100: "#94928D",
+          200: "#AFAFAF",
+          300: "#42424570"
+        },
+        zinc: "#101010"
+      }
+    },
   },
   plugins: [],
 }
