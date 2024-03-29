8463fb948996469bdc40b6c68021595b26ab4199
Jeferson Fernandes <jeferson.fernandes@maxiconsystems.com.br>
2024-03-29 17:38:39 -0300

    FEAT: footer component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Highlights from "./components/Highlights";
 import Model from "./components/Model";
 import Features from "./components/Features";
 import HowItWorks from "./components/HowItWorks";
+import Footer from "./components/Footer";
+
 const App = () => {
   return (
     <main className="bg-black">
@@ -13,6 +15,7 @@ const App = () => {
       <Model />
       <Features />
       <HowItWorks />
+      <Footer />
     </main>
   )
 }
