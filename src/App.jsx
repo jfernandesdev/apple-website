13f94c2799478860c549c88722e6ef23c7bceb43
Jeferson Fernandes <jeferson.fernandes@maxiconsystems.com.br>
2024-03-29 17:06:57 -0300

    FEAT: how it works section (pro chip)

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import Navbar from "./components/Navbar"
-import Hero from "./components/Hero"
-import Highlights from "./components/Highlights"
-import Model from "./components/Model"
-import Features from "./components/Features"
-
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Highlights from "./components/Highlights";
+import Model from "./components/Model";
+import Features from "./components/Features";
+import HowItWorks from "./components/HowItWorks";
 const App = () => {
   return (
     <main className="bg-black">
@@ -12,6 +12,7 @@ const App = () => {
       <Highlights />
       <Model />
       <Features />
+      <HowItWorks />
     </main>
   )
 }
